9b16a7f883ddd92c88b74fa6fb1d58e767fff948
6hislain <ghislainmike80@gmail.com>
2024-04-24 16:36:35 +0200

    first commit

---
+++ main/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from django.views.generic import TemplateView
+from . import views
+
+urlpatterns = [
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("dashboard", views.dashboard, name="dashboard"),
+    path("signup", views.signup, name="signup"),
+    path("signin", views.signin, name="signin"),
+    path("logout", views.logout, name="logout"),
+    # path("article/", views.ArticleList.as_view(), name="articles"),
+    # path("article/<slug:slug>", views.ArticleDetail.as_view(), name="article"),
+    # path("category/<slug:slug>", views.CategoryDetail.as_view(), name="category"),
+    # path("page/<slug:slug>", views.PageDetail.as_view(), name="page"),
+]
