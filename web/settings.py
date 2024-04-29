1abfe2c23295577ee5a083e8555f7cf2fb1e097e
fabricetuyishime <tuyishimefebrice250@gmail.com>
2024-04-29 17:41:08 +0200

    mysql

--- web/settings.py
+++ web/settings.py
@@ -76,9 +76,18 @@ WSGI_APPLICATION = "web.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE":"django.db.backends.mysql",
+        "NAME":"fish health monitoring",
+        "USER": "root",
+        "PASSWORD":"",
+        "HOST":"127.0.0.1",
+        "PORT":"3306",
+        "OPTIONS": {"init_command": "SET sql_mode='STRICT_TRANS_TABLES'"},
     }
 }
 
